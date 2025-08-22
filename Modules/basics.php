986e20f05a95263d6bfd607f35566fe6fabeca7d
KhanterWinters <khanter_winters@hotmail.com>
2025-08-22 14:47:11 +0200

    Create Modules / basics.php
    First module for trial and Error

---
+++ Modules/basics.php
@@ -0,0 +1,23 @@
+<?php
+namespace Modules;
+
+use Discord\Discord;
+use Discord\Parts\Channel\Message;
+
+class PingModule
+{
+    private Discord $bot;
+
+    public function __construct(Discord $bot)
+    {
+        $this->bot = $bot;
+    }
+
+    public function handle(Message $message): void
+    {
+        if ($message->content === '!ping') {
+            $latency = $this->bot->getGateway()->getLatency() * 1000;
+            $message->channel->sendMessage("🏓 Pong {$latency} ms");
+        }
+    }
+}
