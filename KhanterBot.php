de8ecdaa0df5f0ffcb37322f8660475b37ca2b94
KhanterWinters <khanter_winters@hotmail.com>
2025-08-23 15:56:00 +0200

    Update KhanterBot.php

--- KhanterBot.php
+++ KhanterBot.php
@@ -20,7 +20,9 @@ $discord->on('ready', function () use ($manager) {
     $manager->load('DadJoke'); //Typical Joke of "I am"
     $manager->load('LanguageTranslate'); // Find and fit free translate API
     $manager->load('Kingdoms'); // API for Travian Kingdoms
-    $manager->load('Bridge'); // API for Telegram Chat
+    $manager->load('Telegram'); // API for Telegram Chat
+    $bridge = $manager->getLoaded()['Telegram'];
+    $bridge->startTelegramPoller();
     $manager->load('Quotes'); // API for some Inspiring Quotes
 });
 
