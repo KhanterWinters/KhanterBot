47793d1fb0ab7f1d0c6c3342b5cd505e152f1080
KhanterWinters <khanter_winters@hotmail.com>
2025-08-23 14:31:47 +0200

    Create index.php

---
+++ public/index.php
@@ -0,0 +1,23 @@
+<?php
+use React\Http\Message\Response;
+use Psr\Http\Message\ServerRequestInterface;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$loop   = React\EventLoop\Loop::get();
+$socket = new React\Socket\Server('0.0.0.0:' . ($_ENV['PORT'] ?? 4000), $loop);
+$http   = new React\Http\HttpServer(
+    function (ServerRequestInterface $request) {
+        return new Response(
+            200,
+            ['Content-Type' => 'text/plain'],
+            "Bot alive\n"
+        );
+    }
+);
+
+$http->listen($socket);
+echo "HTTP keep-alive listening on {$socket->getAddress()}\n";
+
+// Arrancar también el bot
+require __DIR__ . '/../KhanterBot.php';
